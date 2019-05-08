4e57718d276617b1819d6001b4ee62b72ab162f5
Kevin Liang <kev.liang@utexas.edu>
2019-05-07 22:05:27 -0500

    first iteration of updated style for State, StateMachine, and associated tests

--- tests/src/StateMachineTest.cpp
+++ tests/src/StateMachineTest.cpp
@@ -3,35 +3,40 @@
 #include "StateMachine.hpp"
 #include "Errors.h"
 
-TEST_GROUP(StateMachines) {
+TEST_GROUP (StateMachines) 
+{
 };
 
-TEST(StateMachines, Default_Case) {
-	StateMachine SM = StateMachine::from_default();
-	int resultA;
-	int resultB;
-	Error_t retA = SM.getA(resultA);
-	Error_t retB = SM.getB(resultB);
+/* Test to create a StateMachine from default hardcoded case, then verify StateMachine data */
+TEST (StateMachines, DefaultCase) 
+{
+    StateMachine SM = StateMachine::fromDefault ();
+    int resultA;
+    int resultB;
+    Error_t retA = SM.getA (resultA);
+    Error_t retB = SM.getB (resultB);
 
-	// Default Case returns A = 1, B = 2
-	CHECK_TRUE(E_SUCCESS == retA);
-	CHECK_TRUE(E_SUCCESS == retB);
-	CHECK_EQUAL(1, resultA);
-	CHECK_EQUAL(2, resultB);
+    // Default Case returns A = 1, B = 2
+    CHECK_TRUE (E_SUCCESS == retA);
+    CHECK_TRUE (E_SUCCESS == retB);
+    CHECK_EQUAL (1, resultA);
+    CHECK_EQUAL (2, resultB);
 }
 
-TEST(StateMachines, Defined_Case) {
-	int data_example[] = { 1, 1, 1, 1 };
-	StateMachine SM = StateMachine::from_arr(data_example);
-	int resultA;
-	int resultB;
-	Error_t retA = SM.getA(resultA);
-	Error_t retB = SM.getB(resultB);
+/* Test to create a StateMachine from a defined case using data from an array, then verify internal calculations from data */
+TEST (StateMachines, DefinedCase) 
+{
+    int32_t data_example[] = { 1, 1, 1, 1 };
+    StateMachine SM = StateMachine::fromArr (data_example);
+    int32_t resultA;
+    int32_t resultB;
+    Error_t retA = SM.getA (resultA);
+    Error_t retB = SM.getB (resultB);
 
-	// Defined Case for Array returns A = arr[0], B = sum of arr[0] to arr[3]
-	CHECK_TRUE(E_SUCCESS == retA);
-	CHECK_TRUE(E_SUCCESS == retB);
-	CHECK_EQUAL(1, resultA);
-	CHECK_EQUAL(4, resultB);
+    // Defined Case for Array returns A = arr[0], B = sum of arr[0] to arr[3]
+    CHECK_TRUE (E_SUCCESS == retA);
+    CHECK_TRUE (E_SUCCESS == retB);
+    CHECK_EQUAL (1, resultA);
+    CHECK_EQUAL (4, resultB);
 }
 
--- tests/src/StateTest.cpp
+++ tests/src/StateTest.cpp
@@ -3,17 +3,18 @@
 #include "State.hpp"
 #include "Errors.h"
 
-TEST_GROUP(States) {
+TEST_GROUP (States) 
+{
 };
 
-TEST(States, Access_Data) {
-	std::vector<int> data = { 1, 2, 3, 4 };
-	State S = State(data);
-	std::vector<int> stateData;
-	Error_t ret = S.getData(stateData);
+/* Test to create a State with data, then try to access the State's data */
+TEST (States, AccessData)
+{
+    std::vector<int> data = { 1, 2, 3, 4 };
+    State S = State (data);
+    std::vector<int> stateData;
+    Error_t ret = S.getData (stateData);
 
-	CHECK_TRUE(E_SUCCESS == ret);
-	for (unsigned i = 0; i < data.size(); i++) {
-		CHECK_EQUAL(data.at(i), stateData.at(i));
-	}
+    CHECK_TRUE (E_SUCCESS == ret);
+    CHECK_TRUE (data == stateData);
 }
