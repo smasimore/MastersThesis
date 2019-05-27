c9d21b719ed9c14c41fe7a94be5a24d2bb1638d0
Kevin Liang <kev.liang@utexas.edu>
2019-05-27 14:21:05 -0700

    fixed missed style changes

--- src/StateMachine.cpp
+++ src/StateMachine.cpp
@@ -2,39 +2,46 @@
 
 /******************** PUBLIC FUNCTIONS **************************/
 
-StateMachine StateMachine::fromDefault () {
-	// Arbitrary default declaration
+Error_t StateMachine::fromDefault ()
+{
+	// Create default case and store in param
 	return StateMachine(1, 2);
 }
 
-StateMachine StateMachine::fromArr (int32_t c[]) {
+Error_t StateMachine::fromArr (StateMachine **ppStateMachine, int32_t c[])
+{
 	// Arbitrary calculations to obtain A, B data from array
 	int a = c[0];
 	int b = 0;
 	for (int32_t i = 0; i < 4; i++) {
 		b += c[i];
 	}
+    // Create case from array and store in param
 	return StateMachine (a, b);
 }
 
-Error_t StateMachine::printData () {
+Error_t StateMachine::printData ()
+{
 	printf("A: %" PRId32 "B: %" PRId32 "", StateMachine::a, StateMachine::b);
 	return E_SUCCESS;
 }
 
-Error_t StateMachine::getA (int32_t &result) {
+Error_t StateMachine::getA (int32_t &result)
+{
 	result = StateMachine::a;
 	return E_SUCCESS;
 }
 
-Error_t StateMachine::getB (int32_t &result) {
+Error_t StateMachine::getB (int32_t &result)
+{
 	result = b;
 	return E_SUCCESS;
 }
 
 /******************** PRIVATE FUNCTIONS **************************/
 
-StateMachine::StateMachine (int32_t a, int32_t b) {
+StateMachine::StateMachine (int32_t a, int32_t b)
+{
 	StateMachine::a = a;
 	StateMachine::b = b;
 }
