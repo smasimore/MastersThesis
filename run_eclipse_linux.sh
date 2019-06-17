9b58054695a03a2192499dba1e926f9344dec371
j0hannB <jonathan.baurer@gmail.com>
2019-06-17 17:09:39 -0500

    Added Linux start-eclipse script

---
+++ run_eclipse_linux.sh
@@ -0,0 +1,4 @@
+export NILRT_TOOLCHAIN_PATH='/usr/local/oecore-x86_64/sysroots/'
+export NILRT_CROSS_GCC_PATH='/usr/local/oecore-x86_64/sysroots/x86_64-nilrtsdk-linux/'
+../../eclipse/cpp-2019-03/eclipse/eclipse
+
