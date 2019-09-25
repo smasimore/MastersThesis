491ca17dece31494570b44e80549d3a6a1bafde5
Sarah Masimore <sarahsmasimore@gmail.com>
2019-09-25 16:28:39 -0500

    [ITAR] Update Eclipse settings to use RocketBox paths (#32)
    * update build configuration to use rocketbox and file permissions so they are modifiable by us and eclipse
    
    * 04_Repositories --> Workspaces
    
    * "tentative commit through eclipse"
    
    * add windows paths
    
    * "test commit"
    
    * use absolute paths for linux includes
    
    * use mnt path for cpputest binary
    
    * fix path bug
    
    * add dl to all
    
    * revert tm change

--- run_eclipse_linux.sh
+++ run_eclipse_linux.sh
@@ -1,4 +1,3 @@
 export NILRT_TOOLCHAIN_PATH='/usr/local/oecore-x86_64/sysroots/'
 export NILRT_CROSS_GCC_PATH='/usr/local/oecore-x86_64/sysroots/x86_64-nilrtsdk-linux/'
-~/eclipse/cpp-2019-03/eclipse/eclipse
-
+~/eclipse/$(ls ~/eclipse)/eclipse/eclipse
