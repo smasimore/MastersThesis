792173e425c52f22339a78ef49c596fe6c4f5c8c
Sarah Masimore <sarahsmasimore@gmail.com>
2020-02-13 11:51:51 -0600

    add lab linux computer include paths and only support lab computer paths in run_eclipse_linux.sh (#65)

--- run_eclipse_linux.sh
+++ run_eclipse_linux.sh
@@ -1,3 +1,3 @@
 export NILRT_TOOLCHAIN_PATH='/usr/local/oecore-x86_64/sysroots/'
 export NILRT_CROSS_GCC_PATH='/usr/local/oecore-x86_64/sysroots/x86_64-nilrtsdk-linux/'
-~/eclipse/$(ls ~/eclipse)/eclipse/eclipse
+/opt/eclipse/eclipse
